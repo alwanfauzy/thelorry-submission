f19acbe58fae8b0275a232e979de6fe1e8d28b28
alwanfauzy <alwanfauzi13@gmail.com>
2023-06-01 09:18:20 +0700

    Organize package

--- app/src/main/java/com/alwan/thelorryresto/presentation/main/MainActivity.kt
+++ app/src/main/java/com/alwan/thelorryresto/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alwan.thelorryresto
+package com.alwan.thelorryresto.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alwan.thelorryresto.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
