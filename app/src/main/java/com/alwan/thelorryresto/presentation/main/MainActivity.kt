4a20faa26012d2dd954ecb95cdb785a27d6c0064
alwanfauzy <alwanfauzi13@gmail.com>
2023-06-01 09:19:50 +0700

    Enable viewbinding

--- app/src/main/java/com/alwan/thelorryresto/presentation/main/MainActivity.kt
+++ app/src/main/java/com/alwan/thelorryresto/presentation/main/MainActivity.kt
@@ -3,10 +3,20 @@ package com.alwan.thelorryresto.presentation.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alwan.thelorryresto.R
+import com.alwan.thelorryresto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
